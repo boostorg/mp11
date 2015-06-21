138e4de0a8767636ab1ac4fbf289bd54090e9e4c
Peter Dimov <pdimov@pdimov.com>
2015-06-22 00:06:50 +0300

    Add tests for mp_count, mp_count_if.

--- include/boost/mp11/algorithm.hpp
+++ include/boost/mp11/algorithm.hpp
@@ -96,10 +96,31 @@ namespace detail
 
 template<class L, class V> struct mp_count_impl;
 
+#if !defined( BOOST_NO_CXX11_CONSTEXPR )
+
+constexpr std::size_t cx_plus()
+{
+    return 0;
+}
+
+template<class T1, class... T> constexpr std::size_t cx_plus(T1 t1, T... t)
+{
+    return t1 + cx_plus(t...);
+}
+
 template<template<class...> class L, class... T, class V> struct mp_count_impl<L<T...>, V>
 {
-    using type = mp_plus<std::is_same<T, V>...>;
+    using type = mp_size_t<cx_plus(std::is_same<T, V>::value...)>;
 };
+
+#else
+
+template<template<class...> class L, class... T, class V> struct mp_count_impl<L<T...>, V>
+{
+    using type = mp_size_t<mp_plus<std::is_same<T, V>...>::value>;
+};
+
+#endif
 
 } // namespace detail
 
@@ -111,19 +132,30 @@ namespace detail
 
 template<class L, template<class...> class P> struct mp_count_if_impl;
 
+#if !defined( BOOST_NO_CXX11_CONSTEXPR )
+
+template<template<class...> class L, class... T, template<class...> class P> struct mp_count_if_impl<L<T...>, P>
+{
+    using type = mp_size_t<cx_plus(mp_to_bool<P<T>>::value...)>;
+};
+
+#else
+
 template<template<class...> class L, class... T, template<class...> class P> struct mp_count_if_impl<L<T...>, P>
 {
 #if defined( BOOST_MSVC ) && BOOST_WORKAROUND( BOOST_MSVC, <= 1800 )
 
     template<class T> struct _f { using type = mp_to_bool<P<T>>; };
-    using type = mp_plus<typename _f<T>::type...>;
+    using type = mp_size_t<mp_plus<typename _f<T>::type...>::value>;
 
 #else
 
-    using type = mp_plus<mp_to_bool<P<T>>...>;
+    using type = mp_size_t<mp_plus<mp_to_bool<P<T>>...>::value>;
 
 #endif
 };
+
+#endif
 
 } // namespace detail
 
--- include/boost/mp11/detail/mp_plus.hpp
+++ include/boost/mp11/detail/mp_plus.hpp
@@ -20,44 +20,22 @@ namespace detail
 
 template<class... T> struct mp_plus_impl;
 
-#if defined( BOOST_NO_CXX11_CONSTEXPR )
-
 template<> struct mp_plus_impl<>
 {
-	using type = std::integral_constant<int, 0>;
+    using type = std::integral_constant<int, 0>;
 };
 
 template<class T1, class... T> struct mp_plus_impl<T1, T...>
 {
-	static const/*expr*/ auto _v = T1::value + mp_plus<T...>::value;
-	using type = std::integral_constant< typename std::remove_const<decltype(_v)>::type, _v >;
+    static const/*expr*/ auto _v = T1::value + mp_plus_impl<T...>::type::value;
+    using type = std::integral_constant<typename std::remove_const<decltype(_v)>::type, _v>;
 };
 
 template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class... T> struct mp_plus_impl<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T...>
 {
-	static const/*expr*/ auto _v = T1::value + T2::value + T3::value + T4::value + T5::value + T6::value + T7::value + T8::value + T9::value + T10::value + mp_plus<T...>::value;
-	using type = std::integral_constant< typename std::remove_const<decltype(_v)>::type, _v >;
+    static const/*expr*/ auto _v = T1::value + T2::value + T3::value + T4::value + T5::value + T6::value + T7::value + T8::value + T9::value + T10::value + mp_plus_impl<T...>::type::value;
+    using type = std::integral_constant<typename std::remove_const<decltype(_v)>::type, _v>;
 };
-
-#else
-
-constexpr int cx_plus()
-{
-	return 0;
-}
-
-template<class T1, class... T> constexpr auto cx_plus( T1 t1, T... t ) -> decltype( t1 + cx_plus( t... ) )
-{
-	return t1 + cx_plus( t... );
-}
-
-template<class... T> struct mp_plus_impl
-{
-	static constexpr auto _v = cx_plus( T::value... );
-	using type = std::integral_constant< typename std::remove_const<decltype(_v)>::type, _v >;
-};
-
-#endif
 
 } // namespace detail
 
