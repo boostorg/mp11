9735300f8527f7104ef1bfbf6cd2ecd3b07c196d
Peter Dimov <pdimov@pdimov.com>
2016-11-17 02:01:50 +0200

    Add mp_map_find.

--- include/boost/mp11/map.hpp
+++ include/boost/mp11/map.hpp
@@ -1,21 +1,27 @@
 #ifndef BOOST_MP11_MAP_HPP_INCLUDED
 #define BOOST_MP11_MAP_HPP_INCLUDED
 
-//  Copyright 2015 Peter Dimov.
+//  Copyright 2015, 2016 Peter Dimov.
 //
 //  Distributed under the Boost Software License, Version 1.0.
 //
 //  See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/mp11/detail/mp_map_find.hpp>
+#include <boost/mp11/integral.hpp>
+#include <type_traits>
+
 namespace boost
 {
 
-// mp_map_find<M, K>
 // mp_map_contains<M, K>
+template<class M, class K> using mp_map_contains = mp_not<std::is_same<mp_map_find<M, K>, void>>;
+
 // mp_map_insert<M, T>
 // mp_map_replace<M, T>
 // mp_map_update<M, T, F>
+// mp_map_erase<M, K>
 
 } // namespace boost
 
