e488469e9a2241c42ac1ac50bf5dbfe10ac8bdad
Peter Dimov <pdimov@pdimov.com>
2017-03-24 17:21:38 +0200

    Unroll mp_fold.

--- include/boost/mp11/algorithm.hpp
+++ include/boost/mp11/algorithm.hpp
@@ -758,6 +758,11 @@ template<template<class...> class L, class T1, class... T, class V, template<cla
     using type = typename mp_fold_impl<L<T...>, F<V, T1>, F>::type;
 };
 
+template<template<class...> class L, class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class... T, class V, template<class...> class F> struct mp_fold_impl<L<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T...>, V, F>
+{
+    using type = typename mp_fold_impl<L<T...>, F<F<F<F<F<F<F<F<F<F<V, T1>, T2>, T3>, T4>, T5>, T6>, T7>, T8>, T9>, T10>, F>::type;
+};
+
 } // namespace detail
 
 template<class L, class V, template<class...> class F> using mp_fold = typename detail::mp_fold_impl<L, V, F>::type;
--- test/mp_fold.cpp
+++ test/mp_fold.cpp
@@ -9,6 +9,7 @@
 
 #include <boost/mp11/algorithm.hpp>
 #include <boost/mp11/list.hpp>
+#include <boost/mp11/integral.hpp>
 #include <boost/core/lightweight_test_trait.hpp>
 #include <type_traits>
 #include <tuple>
@@ -51,6 +52,26 @@ int main()
 
     {
         BOOST_TEST_TRAIT_TRUE((std::is_same<mp_fold<std::tuple<X1, X2, X3, X4>, mp_list<>, mp_push_front>, mp_list<X4, X3, X2, X1>>));
+    }
+
+    using boost::mp11::mp_iota_c;
+    using boost::mp11::mp_reverse;
+    using boost::mp11::mp_size_t;
+    using boost::mp11::mp_plus;
+
+    {
+        int const N = 37;
+
+        using L = mp_iota_c<N>;
+
+        using R1 = mp_fold<L, mp_list<>, mp_push_back>;
+        BOOST_TEST_TRAIT_TRUE((std::is_same<R1, L>));
+
+        using R2 = mp_fold<L, mp_list<>, mp_push_front>;
+        BOOST_TEST_TRAIT_TRUE((std::is_same<R2, mp_reverse<L>>));
+
+        using R3 = mp_fold<L, mp_size_t<0>, mp_plus>;
+        BOOST_TEST_TRAIT_TRUE((std::is_same<R3, mp_size_t<N*(N-1)/2>>));
     }
 
     return boost::report_errors();
