b306ab25aa7d648485d57a59d38dd534b7250511
Peter Dimov <pdimov@pdimov.com>
2017-05-24 03:47:24 +0300

    Do not use auto on g++ 4.7 in mp_plus

--- include/boost/mp11/detail/mp_plus.hpp
+++ include/boost/mp11/detail/mp_plus.hpp
@@ -9,6 +9,7 @@
 //  http://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/mp11/detail/config.hpp>
+#include <boost/detail/workaround.hpp>
 #include <type_traits>
 
 namespace boost
@@ -37,6 +38,24 @@ template<> struct mp_plus_impl<>
     using type = std::integral_constant<int, 0>;
 };
 
+#if BOOST_WORKAROUND( BOOST_GCC, < 40800 )
+
+template<class T1, class... T> struct mp_plus_impl<T1, T...>
+{
+    static const decltype(T1::value + mp_plus_impl<T...>::type::value) _v = T1::value + mp_plus_impl<T...>::type::value;
+    using type = std::integral_constant<typename std::remove_const<decltype(_v)>::type, _v>;
+};
+
+template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class... T> struct mp_plus_impl<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T...>
+{
+    static const
+        decltype(T1::value + T2::value + T3::value + T4::value + T5::value + T6::value + T7::value + T8::value + T9::value + T10::value + mp_plus_impl<T...>::type::value)
+        _v = T1::value + T2::value + T3::value + T4::value + T5::value + T6::value + T7::value + T8::value + T9::value + T10::value + mp_plus_impl<T...>::type::value;
+    using type = std::integral_constant<typename std::remove_const<decltype(_v)>::type, _v>;
+};
+
+#else
+
 template<class T1, class... T> struct mp_plus_impl<T1, T...>
 {
     static const auto _v = T1::value + mp_plus_impl<T...>::type::value;
@@ -48,6 +67,8 @@ template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, c
     static const auto _v = T1::value + T2::value + T3::value + T4::value + T5::value + T6::value + T7::value + T8::value + T9::value + T10::value + mp_plus_impl<T...>::type::value;
     using type = std::integral_constant<typename std::remove_const<decltype(_v)>::type, _v>;
 };
+
+#endif
 
 #endif
 
