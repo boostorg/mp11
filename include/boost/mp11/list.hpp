a7485736c7dd155d08a421f937b080064cd38ba0
Peter Dimov <pdimov@pdimov.com>
2017-10-14 17:42:02 +0300

    Add mp_is_list

--- include/boost/mp11/list.hpp
+++ include/boost/mp11/list.hpp
@@ -19,6 +19,24 @@ namespace boost
 namespace mp11
 {
 
+// mp_is_list<L>
+namespace detail
+{
+
+template<class L> struct mp_is_list_impl
+{
+    using type = mp_false;
+};
+
+template<template<class...> class L, class... T> struct mp_is_list_impl<L<T...>>
+{
+    using type = mp_true;
+};
+
+} // namespace detail
+
+template<class L> using mp_is_list = typename detail::mp_is_list_impl<L>::type;
+
 // mp_size<L>
 namespace detail
 {
