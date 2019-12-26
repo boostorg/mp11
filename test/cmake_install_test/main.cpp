22fc4198e52b37a3c7bc120bf289d0feb9e1bc25
Peter Dimov <pdimov@gmail.com>
2019-12-27 00:46:51 +0200

    Add missing copyright/license preambles

--- test/cmake_install_test/main.cpp
+++ test/cmake_install_test/main.cpp
@@ -1,3 +1,7 @@
+// Copyright 2018 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+
 #include <boost/mp11.hpp>
 using namespace boost::mp11;
 
