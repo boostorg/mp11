90f40dd9993fc27dbffdf2361109d7dd59f0db20
Peter Dimov <pdimov@gmail.com>
2020-11-25 21:41:26 +0200

    Add a test for mp_count_if with N=1089

--- test/mp_count_if.cpp
+++ test/mp_count_if.cpp
@@ -6,6 +6,11 @@
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/mp11/detail/config.hpp>
+
+#if BOOST_MP11_MSVC
+# pragma warning( disable: 4503 ) // decorated name length exceeded
+#endif
 
 #include <boost/mp11/algorithm.hpp>
 #include <boost/mp11/list.hpp>
@@ -16,6 +21,10 @@
 #include <utility>
 
 struct X1 {};
+
+using boost::mp11::mp_bool;
+
+template<class T> using is_even = mp_bool< T::value % 2 == 0 >;
 
 int main()
 {
@@ -53,6 +62,17 @@ int main()
         BOOST_TEST_TRAIT_TRUE((std::is_same<mp_count_if<L2, std::is_volatile>, mp_size_t<0>>));
         BOOST_TEST_TRAIT_TRUE((std::is_same<mp_count_if<L2, std::is_const>, mp_size_t<1>>));
         BOOST_TEST_TRAIT_TRUE((std::is_same<mp_count_if<L2, std::is_pointer>, mp_size_t<1>>));
+    }
+
+    {
+        using boost::mp11::mp_iota_c;
+
+        int const N = 1089;
+
+        using L = mp_iota_c<N>;
+        using R = mp_count_if<L, is_even>;
+
+        BOOST_TEST_TRAIT_TRUE((std::is_same<R, mp_size_t<(N + 1) / 2>>));
     }
 
     return boost::report_errors();
