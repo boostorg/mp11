f4c4e6029d6305faae4efe728f371a0bff2a3da3
Ruben Perez <rubenperez038@gmail.com>
2025-01-01 20:43:46 +0100

    Map tests and modularization

--- test/mp_map_find_3.cpp
+++ test/mp_map_find_3.cpp
@@ -8,9 +8,17 @@
 
 // Same as mp_map_find_2.cpp, but with includes reversed
 
+#ifdef BOOST_USE_MODULES
+import std;
+import boost.core;
+import boost.mp11;
+#include <boost/core/lightweight_test_trait_macros.hpp>
+#else
 #include <tuple>
 #include <boost/mp11/map.hpp>
 #include <boost/core/lightweight_test_trait.hpp>
+#endif
+
 
 struct X {};
 struct Y {};
