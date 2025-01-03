0db344057aa6af9c14b4a90bdef5242c99baf4b5
Ruben Perez <rubenperez038@gmail.com>
2025-01-03 18:15:43 +0100

    CI prototype

---
+++ tools/setup_boost_with_modules.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# This is a temporary workaround to make CIs use the
+# "Boost with C++20 modules" proposal, instead of the regular develop branch
+# Call it instead of depinst
+
+from subprocess import run
+
+def main():
+
+    submodules = [
+        ('tools/cmake',         'https://github.com/anarthal/boost-cmake',    'feature/cxx20-modules'),
+        ('libs/headers',        'https://github.com/anarthal/headers',        'feature/cxx20-modules'),
+        ('libs/config',         'https://github.com/anarthal/config',         'feature/cxx20-modules'),
+        ('libs/assert',         'https://github.com/anarthal/assert',         'feature/cxx20-modules'),
+        ('libs/core',           'https://github.com/anarthal/core',           'feature/cxx20-modules'),
+        ('libs/throw_exception','https://github.com/anarthal/throw_exception','feature/cxx20-modules'),
+        ('libs/static_assert',  'https://github.com/boostorg/static_assert',  'develop'),
+    ]
+
+    for submodule, url, branch in submodules:
+        run(["git", "clone", url, "--depth", "1", "-b", branch, submodule])
+
+if __name__ == '__main__':
+    main()
