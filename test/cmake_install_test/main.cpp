269a62170c7da899d4f866846e19704197d6f700
Ruben Perez <rubenperez038@gmail.com>
2025-01-06 19:15:52 +0100

    Guards for add_subdir/install tests

--- test/cmake_install_test/main.cpp
+++ test/cmake_install_test/main.cpp
@@ -2,7 +2,12 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 
+#ifdef BOOST_USE_MODULES
+import boost.mp11;
+#else
 #include <boost/mp11.hpp>
+#endif
+
 using namespace boost::mp11;
 
 int main()
