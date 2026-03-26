efa2a7b55477ccc86445c1cfffb79d768d563f3e
Peter Dimov <pdimov@gmail.com>
2026-03-26 06:15:52 +0200

    Rename mp_map_update_impl_f<>::_f, mp_map_update_impl_f3<>::_f3 to fn, use _q form

--- include/boost/mp11/map.hpp
+++ include/boost/mp11/map.hpp
@@ -61,18 +61,18 @@ namespace detail
 
 template<class T> struct mp_map_update_impl_f
 {
-    template<class U> using _f = std::is_same<mp_first<T>, mp_first<U>>;
+    template<class U> using fn = std::is_same<mp_first<T>, mp_first<U>>;
 };
 
 template<template<class...> class F> struct mp_map_update_impl_f3
 {
-    // _f3<L<X, Y...>> -> L<X, F<X, Y...>>
-    template<class L> using _f3 = mp_assign<L, mp_list<mp_first<L>, mp_rename<L, F> > >;
+    // fn<L<X, Y...>> -> L<X, F<X, Y...>>
+    template<class L> using fn = mp_assign<L, mp_list<mp_first<L>, mp_rename<L, F> > >;
 };
 
 template<class M, class T, template<class...> class F> struct mp_map_update_impl
 {
-    using type = mp_if< mp_map_contains<M, mp_first<T>>, mp_transform_if<mp_map_update_impl_f<T>::template _f, mp_map_update_impl_f3<F>::template _f3, M>, mp_push_back<M, T> >;
+    using type = mp_if< mp_map_contains<M, mp_first<T>>, mp_transform_if_q<mp_map_update_impl_f<T>, mp_map_update_impl_f3<F>, M>, mp_push_back<M, T> >;
 };
 
 } // namespace detail
